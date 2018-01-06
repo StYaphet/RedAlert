7a4e83286fbdd5e1aaf3e5301ad83a514b8b982e
StYaphet <947670170@qq.com>
2018-01-07 01:45:37 +0800

    Initial Commit

---
+++ RedAlert/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RedAlert
+//
+//  Created by 郝一鹏 on 2018/1/7.
+//  Copyright © 2018年 郝一鹏. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ RedAlert/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  RedAlert
+//
+//  Created by 郝一鹏 on 2018/1/7.
+//  Copyright © 2018年 郝一鹏. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
